bbe89e7fc75dcbdb912cd47f63969111297a0b8f
Łukasz Bigorajski <lbg@touk.pl>
2018-07-16 19:55:19 +0200

    Default configuration

--- src/main/java/com/bigos/logbookmetricsdemo/LogbookConfiguration.java
+++ src/main/java/com/bigos/logbookmetricsdemo/LogbookConfiguration.java
@@ -1,22 +1,21 @@
 package com.bigos.logbookmetricsdemo;
 
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.zalando.logbook.*;
 
 import java.util.List;
-
-import static org.zalando.logbook.BodyReplacers.replaceBody;
-import static org.zalando.logbook.Conditions.*;
+import java.util.function.Predicate;
 
 @Configuration
 public class LogbookConfiguration {
 
     @Bean
     public Logbook logbook(
+            final Predicate<RawHttpRequest> condition,
             final List<RawRequestFilter> rawRequestFilters,
+            final List<RawResponseFilter> rawResponseFilters,
             final List<HeaderFilter> headerFilters,
             final List<QueryFilter> queryFilters,
             final List<BodyFilter> bodyFilters,
@@ -24,37 +23,15 @@ public class LogbookConfiguration {
             final List<ResponseFilter> responseFilters) {
         return Logbook.builder()
                 .rawRequestFilters(rawRequestFilters)
+                .rawResponseFilters(rawResponseFilters)
                 .headerFilters(headerFilters)
                 .queryFilters(queryFilters)
                 .bodyFilters(bodyFilters)
                 .requestFilters(requestFilters)
                 .responseFilters(responseFilters)
                 .writer(new DefaultHttpLogWriter(LoggerFactory.getLogger(Logbook.class), DefaultHttpLogWriter.Level.INFO))
-                .formatter(new LogbookShortLogFormatter())
-                .condition(exclude(
-                        requestTo("**/css/**"),
-                        requestTo("**/js/**"),
-                        requestTo("**/images/**"),
-                        requestTo("**/font/**"),
-                        requestTo("**/libsIE/**"),
-                        requestTo("**/*.html"),
-                        requestTo("**/*.png"),
-                        requestTo("/actuator/info")))
-                .rawResponseFilter(
-                        RawResponseFilters.replaceBody(
-                            replaceBody(
-                                    contentType(
-                                            "application/json",
-                                            "text/plain",
-                                            "application/octet-stream",
-                                            "text/html",
-                                            "image/svg+xml",
-                                            "application/javascript",
-                                            "text/css"
-                                    ),
-                                    "Body filtered out"
-                            )
-                        )
-                ).build();
+                .formatter(new DefaultHttpLogFormatter())
+                .condition(condition)
+                .build();
     }
 }
